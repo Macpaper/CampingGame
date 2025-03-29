3a3c0dfe7d8768bc8534b14d5b74cdd53c16c6ac
Mack Benham <macpaper23@yahoo.com>
2025-03-29 12:46:50 -0400

    first commit

---
+++ CampingGame/src/main/java/com.campoid/Window.java
@@ -0,0 +1,9 @@
+
+public class Window {
+    public Window() {
+        JFrame frame = new JFrame("Camping Game hWAEHawhea");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}

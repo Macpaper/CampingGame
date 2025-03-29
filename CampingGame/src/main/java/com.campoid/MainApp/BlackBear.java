7bc13b6fbab40e4be82638b7efeb80ef182edef7
danieljimenez-palacios <daniel815jimenez@gmail.com>
2025-03-29 18:27:24 -0400

    made basic plants and animals

---
+++ CampingGame/src/main/java/com.campoid/MainApp/BlackBear.java
@@ -0,0 +1,28 @@
+package main.java.com.campoid.MainApp;
+
+import javax.swing.text.Position;
+
+public class BlackBear extends Animal implements Predator{
+    public BlackBear(Position pos) {
+        super(pos);
+    }
+    @Override
+    public void move(Direction direction) {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void attack(Animal predator) {
+
+    }
+}
---
+++ CampingGame/src/main/java/com.campoid/MainApp/Predator.java
@@ -0,0 +1,5 @@
+package main.java.com.campoid.MainApp;
+
+public interface Predator {
+    void attack(Animal predator);
+}

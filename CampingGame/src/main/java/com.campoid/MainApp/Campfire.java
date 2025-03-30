169893ec52e10a94fc16741b7b698cf9921b167b
Kamsi Ben Agu <kamsibenagu22@gmail.com>
2025-03-30 11:37:21 -0400

    committing (I FINALLY PUT THE CAMPFIRE DOWN!)

--- CampingGame/src/main/java/com.campoid/MainApp/Campfire.java
+++ CampingGame/src/main/java/com.campoid/MainApp/Campfire.java
@@ -10,8 +10,6 @@ public class Campfire extends Item {
     public void consume() {
         isAlive = false;
         mainApp.player1.crafting = true;
-        System.out.println("Consume Wood");
-        isAlive = false;
         Campfire c = new Campfire(mainApp, mainApp.player1.worldX , mainApp.player1.worldY );
         mainApp.items.add(c);
     }
--- CampingGame/src/main/java/com.campoid/MainApp/Log.java
+++ CampingGame/src/main/java/com.campoid/MainApp/Log.java
@@ -16,8 +16,6 @@ public class Log extends Item {
     public void consume() {
         isAlive = false;
         mainApp.player1.crafting = true;
-        System.out.println("Consume Wood");
-        isAlive = false;
         Campfire c = new Campfire(mainApp, mainApp.player1.worldX , mainApp.player1.worldY );
         mainApp.items.add(c);
     }
